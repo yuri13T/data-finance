70d02328af5f4eee4a9d6b54d688309d8665416e
yuri13T <yuri1308t@gmail.com>
2022-09-01 15:02:16 +0300

    add eslint prettier and do related code updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
-const Navbar = () => {
+function Navbar() {
   const [nav, setNav] = useState(true);
 
   const handleNav = () => {
@@ -21,7 +21,13 @@ const Navbar = () => {
       <div onClick={handleNav} className="block md:hidden">
         {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
-      <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
+      <div
+        className={
+          !nav
+            ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
+            : 'fixed left-[-100%]'
+        }
+      >
         <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">REACT.</h1>
         <ul className="uppercase p-4">
           <li className="p-4 border-b border-gray-600">Home</li>
@@ -33,6 +39,6 @@ const Navbar = () => {
       </div>
     </div>
   );
-};
+}
 
 export default Navbar;
